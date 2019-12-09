83d943b8dabc974a9d657c2b90c002e2aef7933b
StarpoSeven <55917169+StarpoSeven@users.noreply.github.com>
2019-12-09 18:23:16 +0800

    init repo

---
+++ src/main/java/com/zxp/community/community/Controler/HelloControler.java
@@ -0,0 +1,17 @@
+package com.zxp.community.community.Controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloControler {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name , Model model) {
+        model.addAttribute("name",name);
+        return "hello";
+
+    }
+}
