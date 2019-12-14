7fb9d212495c0478a1d754eb34cae8d8024f0d29
zxp <starpo7@126.com>
2019-12-14 22:44:30 +0800

    P12获取到github用户信息

---
+++ src/main/java/com/zxp/community/community/Controler/AuthorizeController.java
@@ -0,0 +1,35 @@
+package com.zxp.community.community.Controler;
+
+import com.zxp.community.community.DTO.AccessTokenDTO;
+import com.zxp.community.community.DTO.GithubUser;
+import com.zxp.community.community.provider.GitHubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GitHubProvider gitHubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state")String state) throws IOException {
+
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("01b944caa4e465cb867f");
+        accessTokenDTO.setClient_secret("be34879062cfb3b8a42e8b00c64cef7554042136");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8687/calback");
+        accessTokenDTO.setState(state);
+        String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = gitHubProvider.getUser(accessToken);
+        System.out.println(user.getBio());
+        return "index";
+    }
+}
