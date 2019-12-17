2a50b8c171b0270f2c110991bf3cb512d651fb59
zxp <starpo7@126.com>
2019-12-17 23:47:15 +0800

    增减发布页publish.html全是前端的实现和调试

---
+++ src/main/java/com/zxp/community/community/Controler/PublishController.java
@@ -0,0 +1,15 @@
+package com.zxp.community.community.Controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PublishController {
+
+    @GetMapping("/publish")
+    private String Publish() {
+
+        return "publish";
+    }
+
+}
